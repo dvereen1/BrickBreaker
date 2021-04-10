296050e0bed9b6e73d36939a049e3b5e8e3bfdb2
Darian Vereen <vereen.darian@gmail.com>
2021-04-09 21:28:42 -0400

    Add base url to php file(s). Update URI paths of all links.

--- brickBreaker.php
+++ brickBreaker.php
@@ -1,5 +1,6 @@
 <?php include_once('headNoNav.php');?> 
         <title>DJV Brick Breaker</title>
+        <base href = "/portfolio/brickBreaker/">
         <link rel = "stylesheet" href = "/CSS/allProjectsModal.css">
         <link rel = "stylesheet" type = "text/css" href ="brickBreaker.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
